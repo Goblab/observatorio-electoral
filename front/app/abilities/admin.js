e7a4f7b6ab98edd9212260f2268ac56176f29f20
Maximiliano Ivan Biondo <mbiondo@goblab.org>
2015-07-01 13:22:55 -0300

    data entry

--- front/app/abilities/admin.js
+++ front/app/abilities/admin.js
@@ -2,6 +2,7 @@ import { Ability } from 'ember-can';
 
 export default Ability.extend({
   canView: function() {
+    return true;
     
     if (!this.get('session.user_id')){
       return false;    
