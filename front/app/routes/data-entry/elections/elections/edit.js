f11705773dd68902ef48be639080a6a439cfc854
Mbiondo <mbiondo@goblab.org>
2015-07-08 19:35:53 -0300

    Refactor

--- front/app/routes/data-entry/elections/elections/edit.js
+++ front/app/routes/data-entry/elections/elections/edit.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+import SaveModelMixin from '../../../../mixins/roles/save-model-mixin';
+export default Ember.Route.extend(AuthenticatedRouteMixin, SaveModelMixin, {
 });
 
