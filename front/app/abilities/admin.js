e37ae5ab9efeed6776d20daf1c387d0c9738c58e
santuan <smrcouto88@gmail.com>
2015-07-09 12:36:58 -0300

    Se maqueto lo primero

--- front/app/abilities/admin.js
+++ front/app/abilities/admin.js
@@ -2,7 +2,6 @@ import { Ability } from 'ember-can';
 
 export default Ability.extend({
   canView: function() {
-    return true;
     
     if (!this.get('session.user_id')){
       return false;    
--- front/server/index.js
+++ front/server/index.js
@@ -8,14 +8,10 @@
 // };
 
 var bodyParser = require('body-parser');
-var globSync   = require('glob').sync;
-var routes     = globSync('./routes/**/*.js', { cwd: __dirname }).map(require);
 
 module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({
     extended: true
   }));
-
-  routes.forEach(function(route) { route(app); });
 };
