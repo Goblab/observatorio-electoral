7802ed83ac2ac3705c9e8ef1a50fa5f30b94f221
MBiondo <mbiondo@goblab.org>
2015-09-11 17:08:49 -0300

    :D

--- front/app/routes/country.js
+++ front/app/routes/country.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
 	model: function (params) {
 		return Ember.RSVP.hash({
 		  country: this.get('store').find('country', params.country_id),
-          elections: this.get('store').find('election', {country: params.country_id})
+          elections: this.get('store').find('election', {country: params.country_id, enabled: true})
      	});
 	}	
 });
--- front/app/routes/index.js
+++ front/app/routes/index.js
@@ -4,7 +4,7 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 export default Ember.Route.extend({
 	model: function () {
 		return Ember.RSVP.hash({
-          lastElections: this.get('store').find('election', {sort: 'date DESC', limit: 6}),
+          lastElections: this.get('store').find('election', {sort: 'date DESC', limit: 6, enabled: true}),
      	});
 	}
 });
