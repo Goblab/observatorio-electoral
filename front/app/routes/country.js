78b973681f49f314cfbf88c625e57867e3e900b4
santuan <smrcouto88@gmail.com>
2015-10-06 17:05:19 -0300

    mas rock

--- front/app/routes/country.js
+++ front/app/routes/country.js
@@ -4,18 +4,22 @@ export default Ember.Route.extend({
 	model: function (params) {
 		return Ember.RSVP.hash({
 		  country: this.get('store').find('country', params.country_id),
-          types: this.get('store').find('election-type')
+          types: this.get('store').find('election-type'),
+          elections: this.get('store').find('election', {country:  params.country_id, enabled: true})
      	});
 	},
 
-	afterModel: function (model) {
+	
+	/*afterModel: function (model) {
 		var t = model.types.objectAt(0);
 		t.set('isActive', true);
 		model.elections = this.get('store').find('election', {country: model.country.get('id'), enabled: true, type: t.get('id')});
 	},
+	*/
 
 	setupController: function (controller, model) {
 		this._super(controller, model);
+		controller.set('allElections', true);
 		this.controllerFor('application').set('model.country', model.country);
 	},
 
@@ -24,10 +28,14 @@ export default Ember.Route.extend({
 	      var filter = { country: country.get('id'), enabled: true };
 
 	      this.get('controller').get('model.types').setEach('isActive', false);
-
+	      this.get('controller').set('allElections', false);
 	      if (this.get('controller.country')) {
-	        filter.type = type.get('id'); 
-	        type.set('isActive', true);
+	      	if (type) {
+	    	   filter.type = type.get('id'); 
+	  	       type.set('isActive', true);
+	      	} else {
+	      		this.get('controller').set('allElections', true);
+	      	}
 	      }
 	      this.get('controller').set('model.elections', this.get('store').find('election', filter))
 	    }		
