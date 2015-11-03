721541e62e89e6ef3d60d1a2466cddded092640f
santuan <smrcouto88@gmail.com>
2015-11-03 16:27:19 -0300

    ya casi

--- front/app/transitions.js
+++ front/app/transitions.js
@@ -24,6 +24,27 @@ export default function(){
 	);
 
 	this.transition(
+		this.fromRoute('index'),
+		this.toRoute('about'),
+		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	this.transition(
+		this.fromRoute('index'),
+		this.toRoute('contact'),
+		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	this.transition(
+		this.fromRoute('about'),
+		this.toRoute('contact'),
+		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	this.transition(
 		this.fromRoute('election'),
 		this.toRoute('country'),
 		this.use('explode', {
