35687e0f335b0698281d05001d2d094daaca6dae
anew-dev <anew31415926@gmail.com>
2015-12-03 14:39:54 -0300

    about

--- front/app/transitions.js
+++ front/app/transitions.js
@@ -41,8 +41,8 @@ export default function(){
 	this.transition(
 		this.fromRoute('about'),
 		this.toRoute('contact'),
-		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
-		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+		this.use('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
 	);
 
 	this.transition(
