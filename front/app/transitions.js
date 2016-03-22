a3d3b587e9fe562d23680072a6ae80e5af205b13
santuan <smrcouto88@gmail.com>
2016-03-22 01:09:47 -0300

    se tuneo reportes

--- front/app/transitions.js
+++ front/app/transitions.js
@@ -46,6 +46,34 @@ export default function(){
 	);
 
 	this.transition(
+		this.fromRoute('contact'),
+		this.toRoute('reports'),
+		this.use('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	this.transition(
+		this.fromRoute('about'),
+		this.toRoute('reports'),
+		this.use('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	this.transition(
+		this.fromRoute('country'),
+		this.toRoute('reports'),
+		this.use('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	this.transition(
+		this.fromRoute('election'),
+		this.toRoute('reports'),
+		this.use('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	this.transition(
 		this.fromRoute('election'),
 		this.toRoute('country'),
 		this.use('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
